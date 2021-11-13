36f9500d5b5601dde655f8c5b48b46b75a010067
Mehmet Küpeli <57587797+mehmetkupeli@users.noreply.github.com>
2021-11-13 05:28:34 +0300

    Implemented Interfaces

--- MarsRover/Models/Concrete/Ground.cs
+++ MarsRover/Models/Concrete/Ground.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using MarsRover.Models.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MarsRover.Models.Concrete
 {
-    public class Ground
+    public class Ground:IGround
     {
+        public Position GroundPosition { get; set; }
+        //Add Constructor 
+        public Ground(Position position)
+        {
+            GroundPosition = position;
+        }
     }
 }
--- MarsRover/Models/Concrete/Position.cs
+++ MarsRover/Models/Concrete/Position.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using MarsRover.Models.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MarsRover.Models.Concrete
 {
-    public class Position
+    public class Position : IPosition
     {
+        public int PosX { get; set; }
+        public int PosY { get; set; }
+        //Add Constructor 
+        public Position(int x, int y)
+        {
+            PosX = x;
+            PosY = y;
+        }
     }
 }
