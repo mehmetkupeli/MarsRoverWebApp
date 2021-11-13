d0ba8812c72fb3ac1d31850b9938e92508f5ebda
Mehmet Küpeli <57587797+mehmetkupeli@users.noreply.github.com>
2021-11-13 05:20:14 +0300

    Edit Service,Route and Added RoverController

---
+++ MarsRover/Controllers/RoverController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarsRover.Controllers
+{
+    public class RoverController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+    }
+}
--- MarsRover/Startup.cs
+++ MarsRover/Startup.cs
@@ -12,10 +12,9 @@ namespace MarsRover
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +29,10 @@ namespace MarsRover
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name:"default",
+                    pattern:"{controller=Rover}/{action=Home}"
+                    );
             });
         }
     }
