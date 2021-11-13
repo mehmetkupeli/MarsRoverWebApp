33bdde549f862a2fb849ad274c8f365a70fdcde0
Mehmet Küpeli <57587797+mehmetkupeli@users.noreply.github.com>
2021-11-13 05:22:56 +0300

    Add Abstract and Concrete Classes

---
+++ MarsRover/Models/Abstract/IRover.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarsRover.Models.Abstract
+{
+    public interface IRover
+    {
+    }
+}
